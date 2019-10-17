94e415eacb24e43a0a298bdc781a06ef0da8b706
otimthomas <totim444@hotmail.co.uk>
2019-10-17 18:56:18 +0300

    Add navbar, movies, series components and enable routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MovieListComponent } from "./movies/movie-list.component";
+import { SeriesListComponent } from './series-list/series-list.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: "movies", component: MovieListComponent }, {path: "series", component: SeriesListComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {MoviesModule} from './movies/movies.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { MoviesModule } from "./movies/movies.module";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BsNavbarComponent } from "./bs-navbar/bs-navbar.component";
+import { SeriesListComponent } from "./series-list/series-list.component";
+import { SeriesDetailsComponent } from "./series-details/series-details.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    SeriesListComponent,
+    SeriesDetailsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
     BrowserAnimationsModule,
     MoviesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
