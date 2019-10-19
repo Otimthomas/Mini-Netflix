6e980c84f0c6b0ff7e028c1168df0680cacae74d
otimthomas <totim444@hotmail.co.uk>
2019-10-19 19:29:55 +0300

    Add signin form with the neccessary css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule } from "@angular/forms";
 import { MoviesModule } from "./movies/movies.module";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -34,7 +35,8 @@ import { SignUpComponent } from "./sign-up/sign-up.component";
     AppRoutingModule,
     NgbModule,
     BrowserAnimationsModule,
-    MoviesModule
+    MoviesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
